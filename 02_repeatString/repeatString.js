5c9556515ac18e7f50d67d7bf96769c4d4119b03
Sanil Khamkar <sanilpsy@gmail.com>
2022-10-13 14:11:51 -0500

    completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(text, value) {
 
+	let txt = ''
+	
+	if(value >= 0){
+		for(let i=0;i<value;i++) {
+		txt += text
+		}
+	}
+	else {
+		txt = 'ERROR'
+	}
+	
+
+	return txt
 };
+
+repeatString('', 10)
 
 // Do not edit below this line
 module.exports = repeatString;
