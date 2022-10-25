a10a85a45fd9aa32561f370a1466be84634fcea5
Sanil Khamkar <sanilpsy@gmail.com>
2022-10-25 16:47:36 -0500

    wip last test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...value) {
 
+	for(const val of value) {
+		if(typeof val == 'number') {
+			for(let i=0;i<arr.length;i++) {	
+				if(arr[i] == val) {
+					arr.splice(i, 1)
+				}
+			}
+		}
+	}
+
+	console.log(arr)
+
+	return arr
 };
+
+
+
+removeFromArray([1, 2, 3], "1", 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
