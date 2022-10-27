0765e8b41bf98ae2aa2c3ad815ef5b599300e5e9
Sanil Khamkar <sanilpsy@gmail.com>
2022-10-27 15:08:40 -0500

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
 
+	let sum = 0
+	let temp = 0
+
+	if(int1 < 0 || int2 < 0 || typeof int1 == 'string' || typeof int2 == 'string' || typeof int1 == 'object' || typeof int2 == 'object') {
+		return 'ERROR'
+	} 
+	else {
+		if(int1 > int2) {
+		temp = int2
+		int2 = int1
+		int1 = temp
+		}
+
+		for(let i=int1; i<=int2; i++) {
+			sum += i 
+		}
+
+		return sum
+	}
+	
 };
+
+sumAll(1, 5)
 
 // Do not edit below this line
 module.exports = sumAll;
