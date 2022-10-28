9aba9ba8f257859b19bffa55bc7d5247e3c8b555
Sanil Khamkar <sanilpsy@gmail.com>
2022-10-28 14:11:24 -0500

    completed exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+	let value = ''
+
+	if(year % 4 == 0 && year % 100 != 0 || year % 4 == 0 && year % 400 == 0) {
+		value = true
+	}
+	else {
+		value = false
+	}
+
+	return value
 };
+
+
+leapYears(1900)
 
 // Do not edit below this line
 module.exports = leapYears;
