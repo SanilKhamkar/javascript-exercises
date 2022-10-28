44d9da290505e90a9aa184cadaf75d6cbc2c461d
Sanil Khamkar <sanilpsy@gmail.com>
2022-10-28 16:28:06 -0500

    wip caluclator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
+const add = function(value1, value2) {
+		
+		return value1 + value2
+};
+
+const subtract = function(value1, value2) {
+	
+	return value1 - value2
+
+};
+
+const sum = function(value1) {
+	
+	return ''
+
+};
+
+const multiply = function(value1) {
+
+	return value1 * value2
+
+};
+
+const power = function(value1, value2) {
+	
+	return Mat.pow(value1,value2)
+
+};
+
+const factorial = function(value1) {
+
+	return ''
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+console.log('Addition: ',add(0,0))
+console.log('Subract: ', subtract(10,4))
+console.log('Sum of numbers: ', sum([7,11]))
+console.log('Multiplication:  ', multiply([2,4])
+console.log('Power: ', power(4,3))
+console.log('Factorial: ', factorial(0))
 
 // Do not edit below this line
 module.exports = {
