423d502193b271b6db0b5d37c4ae108503c80bdb
Sanil Khamkar <sanilpsy@gmail.com>
2022-10-28 15:22:34 -0500

    completed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,33 @@
-const ftoc = function() {
+const ftoc = function(value) {
+
+  const formula = (value - 32) * (5/9) 
+
+  return roundtemp(formula)
 
 };
 
-const ctof = function() {
+const ctof = function(value) {
+
+  const formula = (value * (9/5) + 32)
+
+  return roundtemp(formula)
 
 };
+
+const roundtemp = function(formula) {
+
+  const round = Math.round(formula * 10) / 10
+
+  return round
+
+};
+
+console.log('Fahrenheits to Celsius: ', ftoc(-100))
+console.log('Celsius to Farenheits ', ctof(73.2))
 
 // Do not edit below this line
 module.exports = {
   ftoc,
   ctof
 };
+
