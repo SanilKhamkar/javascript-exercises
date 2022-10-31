a9367fdc00ddf362a5128fe49e24be8fc8bfdcb6
Sanil Khamkar <sanilpsy@gmail.com>
2022-10-30 19:18:23 -0500

    completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (word) {
+
+	word = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() +]/g, "").toLowerCase()
+
+	j = word.length - 1
+
+	let count = 0
+
+	for(let i=0;i<word.length;i++) {
+		if(word[i] == word[j]) {
+			j--
+			count++
+		}
+		else
+			return false
+	}
+
+	if(count == word.length) {
+		return true
+	} 
+
+//return word
 
 };
+
+console.log(palindromes('Animal loots foliated detail of stool lamina.'))
 
 // Do not edit below this line
 module.exports = palindromes;
