1fc85bf10ada1553d8e3c53b1cac2a9b2844f1fd
Sanil Khamkar <sanilpsy@gmail.com>
2022-11-01 13:47:14 -0500

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
 
+	let f1 = 0
+	let f2 = 1
+
+	if(value > 0) {
+		for(let i=0;i<value;i++) {
+			temp = f1
+			f1 = f1 + f2
+			f2 = temp
+		}
+	}
+	else {
+		return 'OOPS'
+	}
+	
+
+	return f1
 };
+
+fibonacci("8")
 
 // Do not edit below this line
 module.exports = fibonacci;
