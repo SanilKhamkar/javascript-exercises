e3c4a850288a997815025caa3a76364947576a4f
Sanil Khamkar <sanilpsy@gmail.com>
2022-11-01 15:37:13 -0500

    complted exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,29 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+	let bookname = []
+	let count = 0
+
+	for(let value of books) {
+		bookname[count] = value['title']
+		count++
+	}
+
+	return bookname
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+getTheTitles(books)
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
